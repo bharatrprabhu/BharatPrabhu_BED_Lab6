4361b683007bc92e14c1cee8b2c5797417b8d62e
bharat <107795998+bharatrprabhu@users.noreply.github.com>
2023-02-09 21:24:03 +0530

    Add files via upload

---
+++ src/test/java/com/gl/secureStudentManagement/SecureStudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.secureStudentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureStudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
