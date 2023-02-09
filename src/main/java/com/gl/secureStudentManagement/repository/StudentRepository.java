c56e5349e7bfd28023a3c2735f02fb0996462330
bharat <107795998+bharatrprabhu@users.noreply.github.com>
2023-02-09 21:18:04 +0530

    Delete src directory

--- src/main/java/com/gl/secureStudentManagement/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.secureStudentManagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.secureStudentManagement.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-
-}
